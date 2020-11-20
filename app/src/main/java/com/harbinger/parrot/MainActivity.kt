a53fa1b4366dd8978116ff7740b4281b496cf96a
suhang <suhang@roobo.com>
2020-11-20 15:50:54 +0800

    anim

--- app/src/main/java/com/harbinger/parrot/MainActivity.kt
+++ app/src/main/java/com/harbinger/parrot/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.harbinger.parrot
 
 import android.Manifest
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -14,9 +17,6 @@ import com.harbinger.parrot.player.PlayListener
 import com.harbinger.parrot.vad.IVADRecorder
 import com.harbinger.parrot.vad.VADListener
 import com.harbinger.parrot.vad.VadRecorder
-import com.harbinger.parrot.vad.VoiceRecorder
-import com.konovalov.vad.VadConfig
-import com.konovalov.vad.VadListener
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
 import pub.devrel.easypermissions.EasyPermissions.PermissionCallbacks
@@ -28,15 +28,18 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks {
     private var vadRecorder: IVADRecorder? = null
     private var audioPlayer: IAudioPlayer? = null
     private var isRecording = false
+    private var playAnimator: ObjectAnimator? = null
+    private var recordAnimator: ObjectAnimator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR //设置状态栏黑色字体
         this.window.statusBarColor = resources.getColor(R.color.white)
         setContentView(R.layout.activity_main)
+        initUI()
+        initAnimator()
         initAudioPlayer()
         initRecorder()
-        initUI()
     }
 
     private fun initUI() {
@@ -59,14 +62,28 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks {
         })
     }
 
+    private fun initAnimator() {
+        playAnimator = ObjectAnimator.ofFloat(parrotIv, "rotation", 360f)
+        playAnimator?.repeatMode = ValueAnimator.RESTART
+        playAnimator?.repeatCount = ObjectAnimator.INFINITE
+        playAnimator?.duration = 2000
+        recordAnimator = ObjectAnimator.ofFloat(parrotIv, "rotation", -360f)
+        recordAnimator?.repeatMode = ValueAnimator.RESTART
+        recordAnimator?.repeatCount = ObjectAnimator.INFINITE
+        recordAnimator?.duration = 2000
+    }
+
     private fun initAudioPlayer() {
         audioPlayer = AudioPlayer(this)
         audioPlayer?.setPlayListener(object : PlayListener {
             override fun onBegin() {
+                recordAnimator?.pause()
+                playAnim(playAnimator)
             }
 
             override fun onComplete() {
                 startRecord()
+                stopAllAnim()
             }
         })
     }
@@ -87,6 +104,7 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks {
                     Log.d(TAG, "bos")
                     runOnUiThread {
                         statusTv.text = "bos"
+                        playAnim(recordAnimator)
                     }
                 }
 
@@ -94,6 +112,7 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks {
                     Log.d(TAG, "eos")
                     stopRecord()
                     runOnUiThread {
+                        stopAllAnim()
                         audioPlayer?.play(recordPath)
                         statusTv.text = "eos"
                     }
@@ -108,6 +127,16 @@ class MainActivity : AppCompatActivity(), PermissionCallbacks {
         }
     }
 
+    private fun playAnim(anim: ObjectAnimator?) {
+        playAnimator?.pause()
+        recordAnimator?.pause()
+        anim?.start()
+    }
+
+    private fun stopAllAnim() {
+        playAnimator?.pause()
+        recordAnimator?.pause()
+    }
 
     private fun startRecord() {
         vadRecorder?.start()
